341f1f1b5fbd51138d4ca7176d6d3624a345d942
henriquelbk <loubackhenrique123@gmail.com>
2023-03-13 22:21:56 -0300

    .

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-const pokemons = [
-  { nome: 'Bulbasaur', tipo: 'grama', vida: 40 },
-  { nome: 'Bellsprout', tipo: 'grama', vida: 20 },
-  { nome: 'Charmander', tipo: 'fogo', vida: 35 },
-  { nome: 'Vulpix', tipo: 'fogo', vida: 25 },
-  { nome: 'Squirtle', tipo: 'água', vida: 45 },
-  { nome: 'Psyduck', tipo: 'água', vida: 25 }
-]
+let arrayTriplicado = []
+
+function triplicarNumeros(arrayDeNumeros) {
+  for (let numero of arrayDeNumeros) {
+    const numeroTriplicado = numero * 3
+    arrayTriplicado.push(numeroTriplicado)
+  }
+  return arrayTriplicado
+}
+
+/* console.log(triplicarNumeros([9, -21, 882, 3, -57])) */
+
+function retornarPares(arrayDeNumeros) {
+  const arrayPares = []
+
+  for(let numero of arrayDeNumeros) {
+    if(numero % 2 === 0){
+    arrayPares.push(numero)
+  }
+}
+  return arrayPares
+}
+
+/* console.log(retornarPares([2, 4, 5, 8, 9])) */
+
+function manipulaNumeros(arrayDeNumeros, callback){
+  const novoArray = callback(arrayDeNumeros)
+  return novoArray
+}
+
+console.log(manipulaNumeros([3, 16, 17, 22, 38, 44], retornarPares))
+console.log(manipulaNumeros([3, 16, 17, 22, 38, 44], triplicarNumeros))
+
+////////////
+
+const numeros1 = [1, 12, 16, 28, 32, 54]
+
+const numerosTriplicados2 = numeros1.map((elemento, index, arrayOriginal) => {
+  return elemento * 3
+})
+
+console.log(numerosTriplicados2)
+
+const soPares = numeros1.filter((elemento) => {
+  return elemento % 2 === 0
+})
+
+console.log(soPares)
